d71bd4c242c1e10d495468052cb3202da8176e70
ZHANGXIN130 <zxin0816@163.com>
2019-12-12 13:54:06 +0800

    package info

---
+++ src/main/java/com/zxin/java/design/strategy/package-info.java
@@ -0,0 +1,15 @@
+package com.zxin.java.design.strategy;
+
+/**
+ * <p>策略模式</p>
+ * <ul>
+ *     <li>1. 策略接口</li>
+ *     <li>2. 策略实现类</li>
+ *     <li>3. 策略持有者 Context</li>
+ * </ul>
+ *
+ * 难点： 在于对策略选择条件的封装
+ * 1. 枚举
+ * 2. MAP
+ *
+ */
